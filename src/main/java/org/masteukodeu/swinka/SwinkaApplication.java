5e04374ad23a18f1a387fc0806b2f663f049a8c7
Piotr Przybylak <piotr.przybylak@gmail.com>
2020-11-19 22:58:06 +0100

    Initial commit

---
+++ src/main/java/org/masteukodeu/swinka/SwinkaApplication.java
@@ -0,0 +1,13 @@
+package org.masteukodeu.swinka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwinkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwinkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/masteukodeu/swinka/SwinkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.masteukodeu.swinka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwinkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
